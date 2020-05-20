aedb61d05614a4dfe758f7e2ab952064dc234130
Marius Aleksander Sletten <marraleks@hotmail.com>
2020-05-20 14:32:14 +0200

    firestore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./App.scss";
+import firebase from './data/firebase'
 
-const App = () => {
+const App = () => { 
+  const [imgs, setImgs] = useState([])
+
+  useEffect(() => {
+    firebase
+    .firestore()
+    .collection('images')
+    .orderBy('test')
+    .onSnapshot(
+      snapshot => setImgs(snapshot.docs)
+    )
+  }, [])
+
+  
+  
+
   return(
-    <>
-      <h1>
-        test
-      </h1>
+    <>  
+      
     </>
   )
 }
---
+++ src/data/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA1DXwIWVK9P19zP0A18PPn_r5t7O9EvxQ",
+    authDomain: "ayo-ogunseinde.firebaseapp.com",
+    databaseURL: "https://ayo-ogunseinde.firebaseio.com",
+    projectId: "ayo-ogunseinde",
+    storageBucket: "ayo-ogunseinde.appspot.com",
+    messagingSenderId: "628164433401",
+    appId: "1:628164433401:web:f54bd5b781817273709928",
+    measurementId: "G-CWYEDG838J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase
